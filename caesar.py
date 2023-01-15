6bc3b5ea22553e4e91a25e31cfda79d4ea9c9767
Manjun Wang <67433019+CodeSEAS@users.noreply.github.com>
2023-01-14 20:05:12 -0500

    update
    update

--- caesar.py
+++ caesar.py
@@ -1,12 +1,17 @@
-
 def encrypt(key,plaintext):
     ciphertext=""
     #YOUR CODE HERE
+    for c in plaintext:
+        # all characters are in upper case
+        trans_c_number = (key + ord(c) - 65) % 26 + 65
+        ciphertext += chr(trans_c_number)
     return ciphertext
 
 def decrypt(key,ciphertext):
     plaintext=""
     #YOUR CODE HERE
+    for c in ciphertext:
+        # all characters are in upper case
+        trans_c_number = (ord(c) - 65 - key) % 26 + 65
+        plaintext += chr(trans_c_number)
     return plaintext
-
-
