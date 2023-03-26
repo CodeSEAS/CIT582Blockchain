633452b98de0efa3e0f904fda61769a64ffa7d03
ManjunWang <67433019+CodeSEAS@users.noreply.github.com>
2023-03-26 17:39:57 -0400

    update

--- AMM.sol
+++ AMM.sol
@@ -52,8 +52,8 @@ contract AMM is AccessControl{
 		require( sellToken == tokenA || sellToken == tokenB, 'Invalid token' );
 		require( sellAmount > 0, 'Cannot trade 0' );
 		require( invariant > 0, 'No liquidity' );
-		uint256 qtyA;
-		uint256 qtyB;
+		uint256 amount;
+		uint256 newSellAmount;
 		uint256 swapAmt;
 
 		//YOUR CODE HERE 
