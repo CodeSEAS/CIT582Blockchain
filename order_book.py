8009b64938e3ff973dff664cefc9f06e2ff3ba21
ManjunWang <67433019+CodeSEAS@users.noreply.github.com>
2023-04-01 21:24:33 -0400

    update

--- order_book.py
+++ order_book.py
@@ -22,11 +22,11 @@ def process_order(order):
     for existing_order in session.query(Order).filter(Order.creator == None).all():
         if existing_order.filled is not None:
             continue
-        if existing_order.buy_currency != new_inserted_order.sell_currency:
-            continue
         if existing_order.sell_currency != new_inserted_order.buy_currency:
             continue
-        if existing_order.sell_amount / existing_order.buy_amount < new_inserted_order.buy_amount/ new_inserted_order.sell_amount:
+        if existing_order.buy_currency != new_inserted_order.sell_currency:
+            continue
+        if existing_order.sell_amount / existing_order.buy_amount < new_inserted_order.buy_amount / new_inserted_order.sell_amount:
             continue
         new_inserted_order.filled = datetime.now()
         existing_order.filled = datetime.now()
@@ -34,19 +34,7 @@ def process_order(order):
         existing_order.counterparty_id = new_inserted_order.id
         session.commit()
 
-        if new_inserted_order.sell_amount > existing_order.buy_amount:
-            new_buy_amount = (new_inserted_order.sell_amount - existing_order.buy_amount) * new_inserted_order.buy_amount/ new_inserted_order.sell_amount
-            new_sell_amount = new_inserted_order.sell_amount - existing_order.buy_amount
-            modified_order = Order(sender_pk = order['sender_pk'], 
-                               receiver_pk = order['receiver_pk'], 
-                               buy_currency = order['buy_currency'], 
-                               sell_currency = order['sell_currency'], 
-                               buy_amount = new_buy_amount, 
-                               sell_amount = new_sell_amount,
-                               creator_id = new_inserted_order.id)
-            session.add(modified_order)
-            session.commit()    
-        elif new_inserted_order.sell_amount < existing_order.buy_amount:
+        if new_inserted_order.sell_amount < existing_order.buy_amount:
             new_buy_amount = existing_order.buy_amount - new_inserted_order.sell_amount
             new_sell_amount = (existing_order.buy_amount - new_inserted_order.sell_amount) * existing_order.sell_amount / existing_order.buy_amount
             modified_order = Order(sender_pk = existing_order.sender_pk, 
@@ -58,6 +46,18 @@ def process_order(order):
                                creator_id = existing_order.id)
             session.add(modified_order)
             session.commit()
+        elif new_inserted_order.sell_amount > existing_order.buy_amount:
+            new_buy_amount = (new_inserted_order.sell_amount - existing_order.buy_amount) * new_inserted_order.buy_amount/ new_inserted_order.sell_amount
+            new_sell_amount = new_inserted_order.sell_amount - existing_order.buy_amount
+            modified_order = Order(sender_pk = order['sender_pk'], 
+                               receiver_pk = order['receiver_pk'], 
+                               buy_currency = order['buy_currency'], 
+                               sell_currency = order['sell_currency'], 
+                               buy_amount = new_buy_amount, 
+                               sell_amount = new_sell_amount,
+                               creator_id = new_inserted_order.id)
+            session.add(modified_order)
+            session.commit()    
         break
     return    
 
